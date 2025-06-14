02374f654fe263f0835645d918239a446dfe6e18
Hyeon <gayoung.h@lgcns.com>
2025-06-14 15:27:57 +0900

    add all dummy pages

--- src/pages/admin/ContentsListPage.tsx
+++ src/pages/admin/ContentsListPage.tsx
@@ -1,3 +1,131 @@
+import {
+  ColumnDef,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
+import BaseAdminPage from './BaseAdminPage';
+import { fetchContentList, Content } from '@/api/adminService';
+import AdminPagination from './AdminPagination';
+import AdminDataTable from './AdminDataTable';
+import AdminDataFilter from './AdminDataFilter';
+
+interface ContentWithId extends Content {
+  id: string;
+}
+
+const columns: ColumnDef<ContentWithId>[] = [
+  {
+    accessorKey: 'totalContentId',
+    header: '컨텐츠 ID',
+    cell: ({ row }) => <div>{row.getValue('totalContentId')}</div>,
+  },
+  {
+    accessorKey: 'contentTitle',
+    header: '제목',
+    cell: ({ row }) => <div>{row.getValue('contentTitle')}</div>,
+  },
+  {
+    accessorKey: 'contentType',
+    header: '유형',
+    cell: ({ row }) => <div>{row.getValue('contentType')}</div>,
+  },
+  {
+    accessorKey: 'contentPlatform',
+    header: '플랫폼',
+    cell: ({ row }) => <div>{row.getValue('contentPlatform')}</div>,
+  },
+  {
+    accessorKey: 'contentDuration',
+    header: '소요 시간',
+    cell: ({ row }) => <div>{row.getValue('contentDuration')}</div>,
+  },
+  {
+    accessorKey: 'contentLevel',
+    header: '난이도',
+    cell: ({ row }) => <div>{row.getValue('contentLevel')}</div>,
+  },
+  {
+    accessorKey: 'contentPrice',
+    header: '가격',
+    cell: ({ row }) => <div>{row.getValue('contentPrice')}</div>,
+  },
+  {
+    accessorKey: 'subId',
+    header: '과목 ID',
+    cell: ({ row }) => <div>{row.getValue('subId')}</div>,
+  },
+];
+
 export default function ContentsListPage() {
-  return <div>ContentsListPage</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageIndex = Number(searchParams.get('page')) || 1;
+
+  const query = useQuery({
+    queryKey: ['contents', { ...searchParams }],
+    queryFn: async () => {
+      const data = await fetchContentList({
+        pageSize: 10,
+        pageIndex: pageIndex - 1,
+        ...searchParams,
+      });
+      return {
+        ...data,
+        content: data.content.map((content) => ({
+          ...content,
+          id: content.totalContentId.toString(),
+        })),
+      };
+    },
+  });
+
+  const table = useReactTable({
+    data: query.data?.content || [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  const setPage = (page: number) => {
+    setSearchParams((prev) => {
+      prev.set('page', page.toString());
+      return prev;
+    });
+  };
+
+  const handleSubmit = (data: { contentTitle: string; contentType: string }) => {
+    setSearchParams((prev) => {
+      data.contentTitle ? prev.set('contentTitle', data.contentTitle) : prev.delete('contentTitle');
+      data.contentType ? prev.set('contentType', data.contentType) : prev.delete('contentType');
+      prev.set('page', '1');
+      return prev;
+    });
+  };
+
+  return (
+    <BaseAdminPage title="컨텐츠 관리">
+      <AdminDataFilter
+        onSubmit={handleSubmit}
+        items={[
+          {
+            name: 'contentTitle',
+            label: '제목',
+          },
+          {
+            name: 'contentType',
+            label: '유형',
+          },
+        ]}
+      />
+      <AdminDataTable table={table} columns={columns} query={query} />
+      <AdminPagination
+        pageIndex={pageIndex}
+        totalPages={query.data?.totalPages || 0}
+        setPage={setPage}
+      />
+    </BaseAdminPage>
+  );
 }
